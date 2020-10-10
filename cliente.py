74337ce45bae5bd748e41e7a13c7186e41a9aff8
APF2000 <arthurpfonseca@gmail.com>
2020-10-10 16:41:03 -0300

    WTF

--- cliente.py
+++ cliente.py
@@ -9,14 +9,20 @@ if len(sys.argv) < 2:
 host = sys.argv[1]
 port = 51432
 
-if 
+#print("oi")
+tudo = socket.gethostbyaddr(host)
+host = tudo[0]
+ip = tudo[2][0]
+
+#print(ip, host)
+#print("oi2")
 
 while 1:
     # Criando o socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
 
     try:
-        s.connect((host, port))
+        s.connect((ip, port))
     except socket.error as e:
         print ("%s : simplex-talk: socket error" % e)
         sys.exit(1)
@@ -29,15 +35,40 @@ while 1:
 
     # Mandando mensagem para o servidor
     try :
-        data = input("Send data to server:\n").encode('utf-8')
+        #data = input("Send data to server:\n").encode('utf-8')
+        archive = input("Nome do arquivo?\n")
+        data = "GET /%s HTTP/1.1\nHost: %s\n\n" % (archive, host)
+        data = data.encode('utf-8')
+
         s.send(data)
+
     except socket.error as e:
         print ("%s : simplex-talk: socket error" % e)
         sys.exit(1)
     except Exception as e:
         print( "Error: %s" % e)    
-        continue 
+        continue
+    
+        #serversock, serveraddr = s.accept()
+        #print ("Got connection from", serversock.getpeername())
+    
+    try:
+        buf = s.recv(256)
+        response = buf.decode('utf-8')
+        content = response.split('\n')[-1]
+    
+        print("Received2: %s" % content)
+    except Exception as e:
+        print( "Error: %s" % e)    
+        continue
 
     s.close()
+  
+  
+  
+  
+  
+  
+  
   
 # Driver code 
--- servidor.py
+++ servidor.py
@@ -1,5 +1,8 @@
 import socket as soc
 import sys
+import mimetypes
+import re
+
 
 host = 'localhost'
 port = 51432
@@ -19,27 +22,44 @@ while 1:
     try:
         clientsock, clientaddr = s.accept()
         print ("Got connection from", clientsock.getpeername())
+
         buf = clientsock.recv(256)
-        print("Received : %s" % buf.decode('utf-8'))
+        request = buf.decode('utf-8')
+        print("Received : %s" % request)
+
+        firstline = request.split('\n')[0]
+        #import pdb; pdb.set_trace()
+        archive_name = re.sub('/', '',firstline.split(' ')[1])
+        print(archive_name)
+
+        archive = open(archive_name, 'r').read()
+
+        status = "HTTP/1.1 200 OK\n"
+        length = "Content-Length: %d\n" % len(archive)
+        content = "Content-Type: %s\n\n" % mimetypes.MimeTypes().guess_type(archive_name)[0]
+        
+        data = status + length + content + archive
+        data = data.encode('utf-8')
+        print(data)
+
+        s.sendall(data)
+
 
     except KeyboardInterrupt:
         raise
         clientsock.close()
 
     except soc.error as e:
+    
         print("%s : simplex-talk: socket" % e)        
         sys.exit(1)
         
     except Exception as e:
         print( "Error: %s" % e)    
         continue 
-    
+   
+    except FileNotFoundError:
+            print("%s not found\n" % archive_name)
+            data = "HTTP/1.1 400 ERRO \n\n"
     clientsock.close()
 
-
-site_name = "www.sitelegal.com.br"
-path = './teste.txt'
-try:
-    archive = open(path, 'r').read()
-except FileNotFoundError:
-    print("%s not found" % path)
